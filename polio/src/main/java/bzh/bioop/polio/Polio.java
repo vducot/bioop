9d6d62a2d770e235468d93f6ffcbec7186efd87e
Vincent <vincent>
2025-11-09 20:58:31 +0100

    Small modif

--- polio/src/main/java/bzh/bioop/polio/Polio.java
+++ polio/src/main/java/bzh/bioop/polio/Polio.java
@@ -22,14 +22,17 @@ public class Polio {
 
     /**
      * Constructor for Polio object
-     * @param citySize Length of a side of the square representing the city
-     * @param density Wanted density of people in the city, between 0 and 1
-     * @param deathProbability Death probability when sick
+     * 
+     * @param citySize          Length of a side of the square representing the city
+     * @param density           Wanted density of people in the city, between 0 and
+     *                          1
+     * @param deathProbability  Death probability when sick
      * @param spreadProbability Spread probability when sick
-     * @param p_vax Vaccine coverage, as a probability to be vaccinated
-     * @param vaxPolioProb Probability a vaccinated person still catches polio
-     * @param moveProbability Probability for people to move at each turn
-     * @param clusters True if the city is mostly populated with clusters of people
+     * @param p_vax             Vaccine coverage, as a probability to be vaccinated
+     * @param vaxPolioProb      Probability a vaccinated person still catches polio
+     * @param moveProbability   Probability for people to move at each turn
+     * @param clusters          True if the city is mostly populated with clusters
+     *                          of people
      * @throws Exception
      */
     public Polio(int citySize, double density, double deathProbability, double spreadProbability,
@@ -54,8 +57,9 @@ public class Polio {
 
     /**
      * Call the global Polio constructor with default values
+     * 
      * @param density Wanted density of people in the city, between 0 and 1
-     * @param p_vax Vaccine coverage, as a probability to be vaccinated
+     * @param p_vax   Vaccine coverage, as a probability to be vaccinated
      * @throws Exception
      */
     public Polio(double density, double p_vax) throws Exception {
@@ -65,6 +69,7 @@ public class Polio {
 
     /**
      * Call the global Polio constructor with default values
+     * 
      * @param density Wanted density of people in the city, between 0 and 1
      * @throws Exception
      */
@@ -76,9 +81,10 @@ public class Polio {
 
     /**
      * Create a city with people mostly grouped by clusters
+     * 
      * @param citySize Length of a side of the square representing the city
-     * @param density Wanted density of people in the city, between 0 and 1
-     * @param p_vax Vaccine coverage, as a probability to be vaccinated
+     * @param density  Wanted density of people in the city, between 0 and 1
+     * @param p_vax    Vaccine coverage, as a probability to be vaccinated
      * @return the initialized city map
      */
     private Person[][] initMatrixWithClusters(int citySize, double density, double p_vax) {
@@ -127,9 +133,10 @@ public class Polio {
 
     /**
      * Create a city with people randomly placed
+     * 
      * @param citySize Length of a side of the square representing the city
-     * @param density Wanted density of people in the city, between 0 and 1
-     * @param p_vax Vaccine coverage, as a probability to be vaccinated
+     * @param density  Wanted density of people in the city, between 0 and 1
+     * @param p_vax    Vaccine coverage, as a probability to be vaccinated
      * @return the initialized city map
      */
     private Person[][] initMatrixWithoutClusters(int citySize, double density, double p_vax) {
@@ -155,6 +162,7 @@ public class Polio {
 
     /**
      * Check if no one is alive in the city
+     * 
      * @return false if at least one person is alive, else true
      */
     public boolean isEndOfTheWorld() {
@@ -175,6 +183,7 @@ public class Polio {
 
     /**
      * Check if at least one person is sick
+     * 
      * @return true is at least one person is sick, else false
      */
     public boolean isOneSick() {
@@ -192,17 +201,26 @@ public class Polio {
 
     /**
      * Infect a person at the position (i, j). Nothing happen if the case is empty.
+     * 
      * @param i The x position in the city
      * @param j The y position in the city
      */
     public void infect(int i, int j) {
         Person p = this.matrix[i][j];
-        if (p != null && !p.isVax()) { // does not infect vaccinated people
-            p.setCurrentState(Person.State.SICK);
-            System.out.println("Infected at position : " + p.getPos_i() + " ; " + p.getPos_j());
+        if (p != null) {
+            if (p.isVax()) {
+                Random rand = new Random();
+                Float x = rand.nextFloat();
+                if (x < 0.15) { // 15% proba to be infected when vaccinated
+                    p.setCurrentState(Person.State.SICK);
+                    System.out.println("Infected at position : " + p.getPos_i() + " ; " + p.getPos_j());
+                }
+            } else { // Not vaccinated, always get sick
+                p.setCurrentState(Person.State.SICK);
+                System.out.println("Infected at position : " + p.getPos_i() + " ; " + p.getPos_j());
+            }
         }
     }
-
 
     /**
      * Infect a person at a random position. Nothing happen if the case is empty.
@@ -213,12 +231,13 @@ public class Polio {
         do {
             i = rand.nextInt(this.getDim());
             j = rand.nextInt(this.getDim());
-        } while (this.matrix[i][j] == null || this.matrix[i][j].isVax());
+        } while (this.matrix[i][j] == null);
         infect(i, j);
     }
 
     /**
      * Check if the specified case has at least one person as neighbor
+     * 
      * @param i The x position in the city
      * @param j The y position in the city
      * @return true if there is at least one neighbor
@@ -232,9 +251,10 @@ public class Polio {
                 { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 }
         };
 
-        for (int[] nb : neighbors) {
-            int ni = i + nb[0];
-            int nj = j + nb[1];
+        //for (int[] nb : neighbors) {
+        for (int nb = 0; nb < neighbors.length - 1 ; nb++) {
+            int ni = i + neighbors[nb][0];
+            int nj = j + neighbors[nb][1];
             if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
                 Person p = matrix[ni][nj];
                 if (p != null) {
@@ -245,14 +265,15 @@ public class Polio {
         return false;
     }
 
-
     /**
-     * Check if the specified case has at least one infected neighbor (sick or carrier)
+     * Check if the specified case has at least one infected neighbor (sick or
+     * carrier)
+     * 
      * @param i The x position in the city
      * @param j The y position in the city
      * @return true if there is at least one infected neighbor
      */
-    public boolean hasNeighborInfectious(int i, int j) {
+    private boolean hasNeighborInfectious(int i, int j) {
         int n = this.getDim();
 
         // Represents the shifts from the given case
@@ -261,9 +282,9 @@ public class Polio {
                 { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 }
         };
 
-        for (int[] nb : neighbors) {
-            int ni = i + nb[0];
-            int nj = j + nb[1];
+        for (int nb = 0; nb < neighbors.length - 1 ; nb++) {
+            int ni = i + neighbors[nb][0];
+            int nj = j + neighbors[nb][1];
             if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
                 Person p = matrix[ni][nj];
                 if (p != null &&
@@ -277,7 +298,9 @@ public class Polio {
 
     /**
      * Find the first empty case in the city
-     * @return The coordinates (x,y) of the first empty case found, or null if no case is empty
+     * 
+     * @return The coordinates (x,y) of the first empty case found, or null if no
+     *         case is empty
      */
     private int[] findEmptyCase() {
         int n = this.getDim();
@@ -296,7 +319,9 @@ public class Polio {
 
     /**
      * Find an empty case in the city when exploring it randomly
-     * @return The coordinates (x,y) of an empty case found, or null if no case is empty (or the function made too much random moves)
+     * 
+     * @return The coordinates (x,y) of an empty case found, or null if no case is
+     *         empty (or the function made too much random moves)
      */
     private int[] findEmptyCase(boolean randomExploration) {
         if (!randomExploration)
@@ -319,9 +344,9 @@ public class Polio {
         return null;
     }
 
-
     /**
      * Compute the next state of a case
+     * 
      * @param i The x position in the city
      * @param j The y position in the city
      * @return A Person with updated status, or null if the case is empty.
@@ -335,7 +360,7 @@ public class Polio {
 
             // if healthy and has an infectious neighbor -> may become sick
             if (p.getCurrentState() == Person.State.HEALTHY && this.hasNeighborInfectious(i, j)) {
-                
+
                 // if vax -> pVaxPolio of being a carrier
                 if (p.isVax()) {
                     if (!p.isCarrier() && rand.nextDouble() < this.getpVaxPolio()) {
@@ -415,6 +440,7 @@ public class Polio {
 
     /**
      * Propagate the polio during n periods
+     * 
      * @param n Number of steps to run
      */
     public void propagatePolio(int n) {
@@ -460,11 +486,11 @@ public class Polio {
         }
     }
 
-
     // Getters and setters
 
     /**
      * Getter for the city size
+     * 
      * @return the size of a city's side
      */
     public int getDim() {
@@ -473,6 +499,7 @@ public class Polio {
 
     /**
      * Getter for death probability
+     * 
      * @return the probability of death when sick
      */
     public double getpDeath() {
@@ -481,6 +508,7 @@ public class Polio {
 
     /**
      * Getter for spread probability
+     * 
      * @return the probability of spread the disease when sick
      */
     public double getpSpread() {
@@ -489,6 +517,7 @@ public class Polio {
 
     /**
      * Getter for vaccinated probability
+     * 
      * @return the probability of being vaccinated
      */
     public double getpVaxPolio() {
@@ -497,6 +526,7 @@ public class Polio {
 
     /**
      * Getter for probability of moving
+     * 
      * @return the probability of moving
      */
     public double getpMove() {
--- polio/src/main/java/bzh/bioop/polio/TestPolio.java
+++ polio/src/main/java/bzh/bioop/polio/TestPolio.java
@@ -7,19 +7,19 @@ public class TestPolio {
     public static void main(String[] args) throws Exception {
 
         //Polio po = new Polio(10, 0.6, 0.65, 0.8, 0.15, 0.05, 0.1, false);
-        Polio po = new Polio(20, 0.7, 0.65, 0.8, 0.15, 0.05, 0.1, true);
-        System.out.println(" Is there any disease " + po.isOneSick());
-        System.out.println(" Is everybody dead yet ? " + po.isEndOfTheWorld());
+        // Polio po = new Polio(20, 0.7, 0.65, 0.8, 0.15, 0.05, 0.1, true);
+        // System.out.println(" Is there any disease " + po.isOneSick());
+        // System.out.println(" Is everybody dead yet ? " + po.isEndOfTheWorld());
 
-        po.infect();
-        po.infect();
-        po.cityDisplay();
-        // System.out.println(po.hasNeighborInfectious(0,0));
-        // System.out.println(po.hasNeighborInfectious(0, 1));
-        // System.out.println(po.hasNeighborInfectious(1, 0));
-        po.propagatePolio(2);
-        System.out.println();
-        po.cityDisplay();
+        // po.infect();
+        // po.infect();
+        // po.cityDisplay();
+        // // System.out.println(po.hasNeighborInfectious(0,0));
+        // // System.out.println(po.hasNeighborInfectious(0, 1));
+        // // System.out.println(po.hasNeighborInfectious(1, 0));
+        // po.propagatePolio(2);
+        // System.out.println();
+        // po.cityDisplay();
 
  
         // System.out.println(po.hasNeighborSick(0, 2));
@@ -28,98 +28,100 @@ public class TestPolio {
         // System.out.println(po.hasNeighborSick(2, 2));
         // System.out.println(po.hasNeighborSick(2, 3));
 
-        // // === Simulation Parameters ===
-        // int citySize = 10;          
-        // double density = 0.8;              // pop density
-        // double pDeath = 0.25; //0.02       // prob of diying when sick
-        // double pSpread = 0.8; //0.3        // prob of spreding polio
-        // double pVax = 0.3; //0.75          // fraction of vaccinated people
-        // double pVaxPolio = 0.1; //0.01     // prop for vax people to transmit polio
-        // boolean enableDisplay = true;     // graphical display
+        // === Simulation Parameters ===
+        int citySize = 10;          
+        double density = 0.7;              // pop density
+        double pDeath = 0.25; //0.02       // prob of diying when sick
+        double pSpread = 0.8; //0.3        // prob of spreding polio
+        double pVax = 0.3; //0.75          // fraction of vaccinated people
+        double pVaxPolio = 0.1; //0.01     // prop for vax people to transmit polio
+        boolean enableDisplay = true;     // graphical display
+        double pMove = 0.5; // prob of move
+        boolean clusters = true;
 
-        // // --- City creation ---
-        // Polio po = new Polio(citySize, density, pDeath, pSpread, pVax, pVaxPolio);
-        // // System.out.println("Initial city:");
-        // // po.cityDisplay();
+        // --- City creation ---
+        Polio po = new Polio(citySize, density, pDeath, pSpread, pVax, pVaxPolio, pMove, clusters);
+        // System.out.println("Initial city:");
+        po.cityDisplay();
 
-        // // Random infection of a non-vaccinated person
-        // po.infect();
+        // Random infection of a non-vaccinated person
+        po.infect();
 
-        // // === Optional Graphical setup ===
-        // EpidemicGrid.EpidemicPanel panel = null;
-        // if (enableDisplay) {
-        //     // initialization
-        //     panel = new EpidemicGrid.EpidemicPanel(citySize, citySize);
-        //     // mark patient zero
-        //     boolean marked = false;
-        //     for (int i = 0; i < citySize && !marked; i++) {
-        //         for (int j = 0; j < citySize && !marked; j++) {
-        //             Person p = po.getMatrix()[i][j];
-        //             if (p != null && p.getCurrentState() == Person.State.SICK) {
-        //                 panel.setPatientZero(i, j);
-        //                 marked = true;
-        //             }
-        //         }
-        //     }
-        //     // create JFrame
-        //     JFrame frame = new JFrame("Polio Epidemic Simulation");
-        //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //     frame.getContentPane().add(new JScrollPane(panel));
-        //     frame.pack();
-        //     frame.setLocationRelativeTo(null);
-        //     frame.setVisible(true);
-        // }
+        // === Optional Graphical setup ===
+        EpidemicGrid.EpidemicPanel panel = null;
+        if (enableDisplay) {
+            // initialization
+            panel = new EpidemicGrid.EpidemicPanel(citySize, citySize);
+            // mark patient zero
+            boolean marked = false;
+            for (int i = 0; i < citySize && !marked; i++) {
+                for (int j = 0; j < citySize && !marked; j++) {
+                    Person p = po.getMatrix()[i][j];
+                    if (p != null && p.getCurrentState() == Person.State.SICK) {
+                        panel.setPatientZero(i, j);
+                        marked = true;
+                    }
+                }
+            }
+            // create JFrame
+            JFrame frame = new JFrame("Polio Epidemic Simulation");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(new JScrollPane(panel));
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        }
 
-        // // === Run the simulation loop ===
-        // int step = 0;
-        // while (po.isOneSick() && !po.isEndOfTheWorld()) {
-        //     step++;
-        //     po.propagatePolio(1);
-        //     // Console display
-        //     System.out.println("\n--- Step " + step + " ---");
-        //     po.cityDisplay();
+        // === Run the simulation loop ===
+        int step = 0;
+        while (po.isOneSick() && !po.isEndOfTheWorld()) {
+            step++;
+            po.propagatePolio(1);
+            // Console display
+            System.out.println("\n--- Step " + step + " ---");
+            po.cityDisplay();
 
-        //     // update the display if enabled
-        //     if (enableDisplay && panel != null) {
-        //         int[][] stateMatrix = new int[citySize][citySize];
-        //         Person[][] matrix = po.getMatrix();
-        //         // convert states to integer for EpidemicGrid
-        //         for (int i = 0; i < citySize; i++) {
-        //             for (int j = 0; j < citySize; j++) {
-        //                 Person p = matrix[i][j];
-        //                 if (p == null) {
-        //                     stateMatrix[i][j] = -1; // empty cell
-        //                 } else {
-        //                     if (p.isVax()) {
-        //                         stateMatrix[i][j] = EpidemicGrid.VAX;
-        //                     } else {
-        //                         switch (p.getCurrentState()) {
-        //                             case HEALTHY -> stateMatrix[i][j] = EpidemicGrid.HEALTHY;
-        //                             case SICK    -> stateMatrix[i][j] = EpidemicGrid.SICK;
-        //                             case DEAD    -> stateMatrix[i][j] = EpidemicGrid.DEAD;
-        //                             case CURED   -> stateMatrix[i][j] = EpidemicGrid.CURED;
-        //                             default      -> stateMatrix[i][j] = -1; // unknown state
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //         panel.setGrid(stateMatrix); // update
-        //         // pause to visualize steps
-        //         try {
-        //             Thread.sleep(500);
-        //         } catch (InterruptedException e) {
-        //             Thread.currentThread().interrupt();
-        //         }
-        //     }
-        // }
+            // update the display if enabled
+            if (enableDisplay && panel != null) {
+                int[][] stateMatrix = new int[citySize][citySize];
+                Person[][] matrix = po.getMatrix();
+                // convert states to integer for EpidemicGrid
+                for (int i = 0; i < citySize; i++) {
+                    for (int j = 0; j < citySize; j++) {
+                        Person p = matrix[i][j];
+                        if (p == null) {
+                            stateMatrix[i][j] = -1; // empty cell
+                        } else {
+                            if (p.isVax()) {
+                                stateMatrix[i][j] = EpidemicGrid.VAX;
+                            } else {
+                                switch (p.getCurrentState()) {
+                                    case HEALTHY -> stateMatrix[i][j] = EpidemicGrid.HEALTHY;
+                                    case SICK    -> stateMatrix[i][j] = EpidemicGrid.SICK;
+                                    case DEAD    -> stateMatrix[i][j] = EpidemicGrid.DEAD;
+                                    case CURED   -> stateMatrix[i][j] = EpidemicGrid.CURED;
+                                    default      -> stateMatrix[i][j] = -1; // unknown state
+                                }
+                            }
+                        }
+                    }
+                }
+                panel.setGrid(stateMatrix); // update
+                // pause to visualize steps
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
 
-        // // === Simulation finished ===
-        // System.out.println("\n=== Simulation ended after " + step + " steps ===");
-        // if (po.isEndOfTheWorld()) {
-        //     System.out.println("Everyone is dead.");
-        // } else {
-        //     System.out.println("Epidemic is over (no one is sick).");
-        // }
+        // === Simulation finished ===
+        System.out.println("\n=== Simulation ended after " + step + " steps ===");
+        if (po.isEndOfTheWorld()) {
+            System.out.println("Everyone is dead.");
+        } else {
+            System.out.println("Epidemic is over (no one is sick).");
+        }
     }
 }
